49f2516ac603e72c933d2723ba9053f625368a99
Graham Fairweather <xotic750@gmail.com>
2019-07-26 16:20:05 +0200

    :bookmark: v3.0.17
    Auto build

--- dist/array-filter-x.js
+++ dist/array-filter-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-25T22:08:06.453Z",
+  "date": "2019-07-26T14:19:58.653Z",
   "describe": "",
   "description": "Creates an array with all elements that pass the test by the provided function.",
   "file": "array-filter-x.js",
   "hash": "84f3daa3203c33136ede",
   "license": "MIT",
-  "version": "3.0.16"
+  "version": "3.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
