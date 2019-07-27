2b9f934fcaa0346c3b24ec0a9b69b90122c797a6
Graham Fairweather <xotic750@gmail.com>
2019-07-27 13:02:17 +0200

    Refactor

--- dist/array-filter-x.esm.js
+++ dist/array-filter-x.esm.js
@@ -1,151 +1,122 @@
-var _this = this;
-
-function _newArrowCheck(innerThis, boundThis) { if (innerThis !== boundThis) { throw new TypeError("Cannot instantiate an arrow function"); } }
-
 import attempt from 'attempt-x';
 import splitIfBoxedBug from 'split-if-boxed-bug-x';
 import toLength from 'to-length-x';
 import toObject from 'to-object-x';
 import assertIsFunction from 'assert-is-function-x';
-/** @type {ArrayConstructor} */
+import requireObjectCoercible from 'require-object-coercible-x';
+var nf = [].filter;
+var nativeFilter = typeof nf === 'function' && nf;
 
-var ArrayCtr = [].constructor;
-/** @type {ObjectConstructor} */
-
-var castObject = {}.constructor;
-/** @type {BooleanConstructor} */
-
-var castBoolean = true.constructor;
-var nativFilter = typeof ArrayCtr.prototype.filter === 'function' && ArrayCtr.prototype.filter;
-var isWorking;
-
-if (nativFilter) {
+var test1 = function test1() {
   var spy = 0;
-  var res = attempt.call([1, 2], nativFilter, function (item) {
-    _newArrowCheck(this, _this);
-
+  var res = attempt.call([1, 2], nativeFilter, function spyAdd1(item) {
     spy += item;
     return false;
-  }.bind(this));
-  isWorking = res.threw === false && res.value && res.value.length === 0 && spy === 3;
+  });
+  return res.threw === false && res.value && res.value.length === 0 && spy === 3;
+};
 
-  if (isWorking) {
-    spy = '';
-    res = attempt.call(castObject('abc'), nativFilter, function (item, index) {
-      _newArrowCheck(this, _this);
+var test2 = function test2() {
+  var spy = '';
+  var res = attempt.call({}.constructor('abc'), nativeFilter, function spyAdd2(item, index) {
+    spy += item;
+    return index === 1;
+  });
+  return res.threw === false && res.value && res.value.length === 1 && res.value[0] === 'b' && spy === 'abc';
+};
 
-      spy += item;
-      return index === 1;
-    }.bind(this));
-    isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === 'b' && spy === 'abc';
+var test3 = function test3() {
+  var spy = 0;
+  var res = attempt.call(function getArgs() {
+    /* eslint-disable-next-line prefer-rest-params */
+    return arguments;
+  }(1, 2, 3), nativeFilter, function spyAdd3(item, index) {
+    spy += item;
+    return index === 2;
+  });
+  return res.threw === false && res.value && res.value.length === 1 && res.value[0] === 3 && spy === 6;
+};
+
+var test4 = function test4() {
+  var spy = 0;
+  var res = attempt.call({
+    0: 1,
+    1: 2,
+    3: 3,
+    4: 4,
+    length: 4
+  }, nativeFilter, function spyAdd4(item) {
+    spy += item;
+    return false;
+  });
+  return res.threw === false && res.value && res.value.length === 0 && spy === 6;
+};
+
+var test5 = function test5() {
+  var doc = typeof document !== 'undefined' && document;
+
+  if (doc) {
+    var spy = null;
+    var fragment = doc.createDocumentFragment();
+    var div = doc.createElement('div');
+    fragment.appendChild(div);
+    var res = attempt.call(fragment.childNodes, nativeFilter, function spyAssign(item) {
+      spy = item;
+      return item;
+    });
+    return res.threw === false && res.value && res.value.length === 1 && res.value[0] === div && spy === div;
   }
 
-  if (isWorking) {
-    spy = 0;
-    res = attempt.call(function getArgs() {
-      /* eslint-disable-next-line prefer-rest-params */
-      return arguments;
-    }(1, 2, 3), nativFilter, function (item, index) {
-      _newArrowCheck(this, _this);
+  return true;
+};
 
-      spy += item;
-      return index === 2;
-    }.bind(this));
-    isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === 3 && spy === 6;
-  }
+var test6 = function test6() {
+  var isStrict = function returnIsStrict() {
+    /* eslint-disable-next-line babel/no-invalid-this */
+    return true.constructor(this) === false;
+  }();
 
-  if (isWorking) {
-    spy = 0;
-    res = attempt.call({
-      0: 1,
-      1: 2,
-      3: 3,
-      4: 4,
-      length: 4
-    }, nativFilter, function (item) {
-      _newArrowCheck(this, _this);
-
-      spy += item;
-      return false;
-    }.bind(this));
-    isWorking = res.threw === false && res.value && res.value.length === 0 && spy === 6;
-  }
-
-  if (isWorking) {
-    var doc = typeof document !== 'undefined' && document;
-
-    if (doc) {
-      spy = null;
-      var fragment = doc.createDocumentFragment();
-      var div = doc.createElement('div');
-      fragment.appendChild(div);
-      res = attempt.call(fragment.childNodes, nativFilter, function (item) {
-        _newArrowCheck(this, _this);
-
-        spy = item;
-        return item;
-      }.bind(this));
-      isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === div && spy === div;
-    }
-  }
-
-  if (isWorking) {
-    var isStrict = function returnIsStrict() {
+  if (isStrict) {
+    var spy = null;
+    var res = attempt.call([1], nativeFilter, function testThis() {
       /* eslint-disable-next-line babel/no-invalid-this */
-      return castBoolean(this) === false;
-    }();
-
-    if (isStrict) {
-      spy = null;
-      res = attempt.call([1], nativFilter, function () {
-        _newArrowCheck(this, _this);
-
-        /* eslint-disable-next-line babel/no-invalid-this */
-        spy = typeof this === 'string';
-      }.bind(this), 'x');
-      isWorking = res.threw === false && res.value && res.value.length === 0 && spy === true;
-    }
+      spy = typeof this === 'string';
+    }, 'x');
+    return res.threw === false && res.value && res.value.length === 0 && spy === true;
   }
 
-  if (isWorking) {
-    spy = {};
-    var fn = ['return nativFilter.call("foo", function (_, __, context) {', 'if (castBoolean(context) === false || typeof context !== "object") {', 'spy.value = true;}});'].join('');
-    /* eslint-disable-next-line no-new-func */
+  return true;
+};
 
-    res = attempt(Function('nativFilter', 'spy', 'castBoolean', fn), nativFilter, spy);
-    isWorking = res.threw === false && res.value && res.value.length === 0 && spy.value !== true;
-  }
-}
-/**
- * This method creates a new array with all elements that pass the test
- * implemented by the provided function.
- *
- * @param {Array} array - The array to iterate over.
- * @param {Function} callBack - Function is a predicate, to test each element.
- * @param {*} [thisArg] - Value to use as this when executing callback.
- * @throws {TypeError} If array is null or undefined.
- * @throws {TypeError} If callBack is not a function.
- * @returns {Array} A new array with the elements that pass the test.
- */
+var test7 = function test7() {
+  var spy = {};
+  var fn = 'return nativeFilter.call("foo", function (_, __, context) {' + 'if (castBoolean(context) === false || typeof context !== "object") {' + 'spy.value = true;}});';
+  /* eslint-disable-next-line no-new-func */
 
+  var res = attempt(Function('nativeFilter', 'spy', 'castBoolean', fn), nativeFilter, spy, true.constructor);
+  return res.threw === false && res.value && res.value.length === 0 && spy.value !== true;
+};
 
-var $filter;
+var isWorking = true.constructor(nativeFilter) && test1() && test2() && test3() && test4() && test5() && test6() && test7();
 
-if (nativFilter) {
-  $filter = function filter(array, callBack
+var patchedFilter = function patchedFilter() {
+  return function filter(array, callBack
   /* , thisArg */
   ) {
-    var args = [callBack];
+    requireObjectCoercible(array);
+    var args = [assertIsFunction(callBack)];
 
     if (arguments.length > 2) {
       /* eslint-disable-next-line prefer-rest-params,prefer-destructuring */
       args[1] = arguments[2];
     }
 
-    return nativFilter.apply(array, args);
+    return nativeFilter.apply(array, args);
   };
-} else {
-  $filter = function filter(array, callBack
+};
+
+var implementation = function implementation() {
+  return function filter(array, callBack
   /* , thisArg */
   ) {
     var object = toObject(array); // If no callback function or if callback is not a callable function
@@ -153,13 +124,9 @@ if (nativFilter) {
     assertIsFunction(callBack);
     var iterable = splitIfBoxedBug(object);
     var length = toLength(iterable.length);
-    var thisArg;
+    /* eslint-disable-next-line prefer-rest-params,no-void */
 
-    if (arguments.length > 2) {
-      /* eslint-disable-next-line prefer-rest-params,prefer-destructuring */
-      thisArg = arguments[2];
-    }
-
+    var thisArg = arguments.length > 2 ? arguments[2] : void 0;
     var noThis = typeof thisArg === 'undefined';
     var result = [];
 
@@ -175,9 +142,21 @@ if (nativFilter) {
 
     return result;
   };
-}
+};
+/**
+ * This method creates a new array with all elements that pass the test
+ * implemented by the provided function.
+ *
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} callBack - Function is a predicate, to test each element.
+ * @param {*} [thisArg] - Value to use as this when executing callback.
+ * @throws {TypeError} If array is null or undefined.
+ * @throws {TypeError} If callBack is not a function.
+ * @returns {Array} A new array with the elements that pass the test.
+ */
 
-var arrayFilter = $filter;
-export default arrayFilter;
+
+var $filter = isWorking ? patchedFilter() : implementation();
+export default $filter;
 
 //# sourceMappingURL=array-filter-x.esm.js.map
--- src/array-filter-x.js
+++ src/array-filter-x.js
@@ -3,170 +3,154 @@ import splitIfBoxedBug from 'split-if-boxed-bug-x';
 import toLength from 'to-length-x';
 import toObject from 'to-object-x';
 import assertIsFunction from 'assert-is-function-x';
+import requireObjectCoercible from 'require-object-coercible-x';
 
-/** @type {ArrayConstructor} */
-const ArrayCtr = [].constructor;
-/** @type {ObjectConstructor} */
-const castObject = {}.constructor;
-/** @type {BooleanConstructor} */
-const castBoolean = true.constructor;
-const nativFilter = typeof ArrayCtr.prototype.filter === 'function' && ArrayCtr.prototype.filter;
+const nf = [].filter;
+const nativeFilter = typeof nf === 'function' && nf;
 
-let isWorking;
-
-if (nativFilter) {
+const test1 = function test1() {
   let spy = 0;
-  let res = attempt.call([1, 2], nativFilter, (item) => {
+  const res = attempt.call([1, 2], nativeFilter, function spyAdd1(item) {
     spy += item;
 
     return false;
   });
 
-  isWorking = res.threw === false && res.value && res.value.length === 0 && spy === 3;
+  return res.threw === false && res.value && res.value.length === 0 && spy === 3;
+};
 
-  if (isWorking) {
-    spy = '';
-    res = attempt.call(castObject('abc'), nativFilter, (item, index) => {
+const test2 = function test2() {
+  let spy = '';
+  const res = attempt.call({}.constructor('abc'), nativeFilter, function spyAdd2(item, index) {
+    spy += item;
+
+    return index === 1;
+  });
+
+  return res.threw === false && res.value && res.value.length === 1 && res.value[0] === 'b' && spy === 'abc';
+};
+
+const test3 = function test3() {
+  let spy = 0;
+  const res = attempt.call(
+    (function getArgs() {
+      /* eslint-disable-next-line prefer-rest-params */
+      return arguments;
+    })(1, 2, 3),
+    nativeFilter,
+    function spyAdd3(item, index) {
       spy += item;
 
-      return index === 1;
+      return index === 2;
+    },
+  );
+
+  return res.threw === false && res.value && res.value.length === 1 && res.value[0] === 3 && spy === 6;
+};
+
+const test4 = function test4() {
+  let spy = 0;
+  const res = attempt.call(
+    {
+      0: 1,
+      1: 2,
+      3: 3,
+      4: 4,
+      length: 4,
+    },
+    nativeFilter,
+    function spyAdd4(item) {
+      spy += item;
+
+      return false;
+    },
+  );
+
+  return res.threw === false && res.value && res.value.length === 0 && spy === 6;
+};
+
+const test5 = function test5() {
+  const doc = typeof document !== 'undefined' && document;
+
+  if (doc) {
+    let spy = null;
+    const fragment = doc.createDocumentFragment();
+    const div = doc.createElement('div');
+    fragment.appendChild(div);
+    const res = attempt.call(fragment.childNodes, nativeFilter, function spyAssign(item) {
+      spy = item;
+
+      return item;
     });
 
-    isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === 'b' && spy === 'abc';
+    return res.threw === false && res.value && res.value.length === 1 && res.value[0] === div && spy === div;
   }
 
-  if (isWorking) {
-    spy = 0;
-    res = attempt.call(
-      (function getArgs() {
-        /* eslint-disable-next-line prefer-rest-params */
-        return arguments;
-      })(1, 2, 3),
-      nativFilter,
-      (item, index) => {
-        spy += item;
+  return true;
+};
 
-        return index === 2;
+const test6 = function test6() {
+  const isStrict = (function returnIsStrict() {
+    /* eslint-disable-next-line babel/no-invalid-this */
+    return true.constructor(this) === false;
+  })();
+
+  if (isStrict) {
+    let spy = null;
+    const res = attempt.call(
+      [1],
+      nativeFilter,
+      function testThis() {
+        /* eslint-disable-next-line babel/no-invalid-this */
+        spy = typeof this === 'string';
       },
+      'x',
     );
 
-    isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === 3 && spy === 6;
+    return res.threw === false && res.value && res.value.length === 0 && spy === true;
   }
 
-  if (isWorking) {
-    spy = 0;
-    res = attempt.call(
-      {
-        0: 1,
-        1: 2,
-        3: 3,
-        4: 4,
-        length: 4,
-      },
-      nativFilter,
-      (item) => {
-        spy += item;
+  return true;
+};
 
-        return false;
-      },
-    );
+const test7 = function test7() {
+  const spy = {};
+  const fn =
+    'return nativeFilter.call("foo", function (_, __, context) {' +
+    'if (castBoolean(context) === false || typeof context !== "object") {' +
+    'spy.value = true;}});';
 
-    isWorking = res.threw === false && res.value && res.value.length === 0 && spy === 6;
-  }
+  /* eslint-disable-next-line no-new-func */
+  const res = attempt(Function('nativeFilter', 'spy', 'castBoolean', fn), nativeFilter, spy, true.constructor);
 
-  if (isWorking) {
-    const doc = typeof document !== 'undefined' && document;
+  return res.threw === false && res.value && res.value.length === 0 && spy.value !== true;
+};
 
-    if (doc) {
-      spy = null;
-      const fragment = doc.createDocumentFragment();
-      const div = doc.createElement('div');
-      fragment.appendChild(div);
-      res = attempt.call(fragment.childNodes, nativFilter, (item) => {
-        spy = item;
+const isWorking = true.constructor(nativeFilter) && test1() && test2() && test3() && test4() && test5() && test6() && test7();
 
-        return item;
-      });
-
-      isWorking = res.threw === false && res.value && res.value.length === 1 && res.value[0] === div && spy === div;
-    }
-  }
-
-  if (isWorking) {
-    const isStrict = (function returnIsStrict() {
-      /* eslint-disable-next-line babel/no-invalid-this */
-      return castBoolean(this) === false;
-    })();
-
-    if (isStrict) {
-      spy = null;
-      res = attempt.call(
-        [1],
-        nativFilter,
-        () => {
-          /* eslint-disable-next-line babel/no-invalid-this */
-          spy = typeof this === 'string';
-        },
-        'x',
-      );
-
-      isWorking = res.threw === false && res.value && res.value.length === 0 && spy === true;
-    }
-  }
-
-  if (isWorking) {
-    spy = {};
-    const fn = [
-      'return nativFilter.call("foo", function (_, __, context) {',
-      'if (castBoolean(context) === false || typeof context !== "object") {',
-      'spy.value = true;}});',
-    ].join('');
-
-    /* eslint-disable-next-line no-new-func */
-    res = attempt(Function('nativFilter', 'spy', 'castBoolean', fn), nativFilter, spy);
-
-    isWorking = res.threw === false && res.value && res.value.length === 0 && spy.value !== true;
-  }
-}
-
-/**
- * This method creates a new array with all elements that pass the test
- * implemented by the provided function.
- *
- * @param {Array} array - The array to iterate over.
- * @param {Function} callBack - Function is a predicate, to test each element.
- * @param {*} [thisArg] - Value to use as this when executing callback.
- * @throws {TypeError} If array is null or undefined.
- * @throws {TypeError} If callBack is not a function.
- * @returns {Array} A new array with the elements that pass the test.
- */
-let $filter;
-
-if (nativFilter) {
-  $filter = function filter(array, callBack /* , thisArg */) {
-    const args = [callBack];
+const patchedFilter = function patchedFilter() {
+  return function filter(array, callBack /* , thisArg */) {
+    requireObjectCoercible(array);
+    const args = [assertIsFunction(callBack)];
 
     if (arguments.length > 2) {
       /* eslint-disable-next-line prefer-rest-params,prefer-destructuring */
       args[1] = arguments[2];
     }
 
-    return nativFilter.apply(array, args);
+    return nativeFilter.apply(array, args);
   };
-} else {
-  $filter = function filter(array, callBack /* , thisArg */) {
+};
+
+const implementation = function implementation() {
+  return function filter(array, callBack /* , thisArg */) {
     const object = toObject(array);
     // If no callback function or if callback is not a callable function
     assertIsFunction(callBack);
     const iterable = splitIfBoxedBug(object);
     const length = toLength(iterable.length);
-    let thisArg;
-
-    if (arguments.length > 2) {
-      /* eslint-disable-next-line prefer-rest-params,prefer-destructuring */
-      thisArg = arguments[2];
-    }
-
+    /* eslint-disable-next-line prefer-rest-params,no-void */
+    const thisArg = arguments.length > 2 ? arguments[2] : void 0;
     const noThis = typeof thisArg === 'undefined';
     const result = [];
     for (let i = 0; i < length; i += 1) {
@@ -181,8 +165,19 @@ if (nativFilter) {
 
     return result;
   };
-}
+};
 
-const arrayFilter = $filter;
+/**
+ * This method creates a new array with all elements that pass the test
+ * implemented by the provided function.
+ *
+ * @param {Array} array - The array to iterate over.
+ * @param {Function} callBack - Function is a predicate, to test each element.
+ * @param {*} [thisArg] - Value to use as this when executing callback.
+ * @throws {TypeError} If array is null or undefined.
+ * @throws {TypeError} If callBack is not a function.
+ * @returns {Array} A new array with the elements that pass the test.
+ */
+const $filter = isWorking ? patchedFilter() : implementation();
 
-export default arrayFilter;
+export default $filter;
