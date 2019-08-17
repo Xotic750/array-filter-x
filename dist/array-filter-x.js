b467de25874f0f6ff22d120e7106ca137f432750
Graham Fairweather <xotic750@gmail.com>
2019-08-18 00:01:18 +0200

    Maintenance

--- dist/array-filter-x.js
+++ dist/array-filter-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-17T12:39:54.856Z",
+  "date": "2019-08-17T22:01:12.040Z",
   "describe": "",
   "description": "Creates an array with all elements that pass the test by the provided function.",
   "file": "array-filter-x.js",
